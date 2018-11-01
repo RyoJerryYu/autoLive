b81aef74ed55ce2634fe3c5eb4a6ce7dbb7a06e0
RyoJerryYu <tzl19961216@gmail.com>
2018-11-01 18:40:24 +0800

    Update: 增加保存设置项页面view
    Fix: 修正view中获得POST数据格式

--- src/webSite/views/autoLive.py
+++ src/webSite/views/autoLive.py
@@ -2,9 +2,10 @@ from flask import Blueprint
 from flask import render_template, redirect, url_for, abort
 from flask import request as flRequest
 
-from src.webSite.models.autoLive import header_menus, schedule_sections
+from src.webSite.models.autoLive import header_menus, schedule_sections, configs_sections
 from src.liveScheduler import LiveScheduler, Live
 from src.rebroadcast import rebroadcast
+from src.Configs import CONFIGs
 
 mod = Blueprint('autoLive', __name__, url_prefix='/autoLive')
 
@@ -44,4 +45,32 @@ def schedule():
         header_menus=header_menus(), 
         form_action=url_for('autoLive.schedule'), 
         sections=schedule_sections()
+    )
+
+@mod.route('/configs', methods=['GET', 'POST'])
+def configs():
+    '''设置页面
+
+    POST访问时动作由参数Action值决定
+    为Apply时应用form中设置
+    为Reset时丢弃form中设置
+    '''
+    if flRequest.method == 'POST' and flRequest.form['Action'] == 'Apply':
+        config = CONFIGs()
+
+        config.BILIBILI_ROOM_TITLE = flRequest.form['BILIBILI_ROOM_TITLE']
+        config.DEFAULT_TITLE_PARAM = flRequest.form['DEFAULT_TITLE_PARAM']
+        config.BILIBILI_ROOM_AREA_ID = int(flRequest.form['BILIBILI_ROOM_AREA_ID'])
+        config.IS_SEND_DAILY_DYNAMIC = True if flRequest.form.get('IS_SEND_DAILY_DYNAMIC') else False
+        config.DAILY_DYNAMIC_FORM = flRequest.form['DAILY_DYNAMIC_FORM']
+        config.IS_SEND_PRELIVE_DYNAMIC = True if flRequest.form.get('IS_SEND_PRELIVE_DYNAMIC') else False
+        config.PRELIVE_DYNAMIC_FORM = flRequest.form['PRELIVE_DYNAMIC_FORM']
+
+        config.LIVE_QUALITY = flRequest.form['LIVE_QUALITY']
+        config.save_configs()
+    return render_template(
+        'configs.html',
+        header_menus=header_menus(),
+        form_action=url_for('autoLive.configs'),
+        sections=configs_sections()
     )
